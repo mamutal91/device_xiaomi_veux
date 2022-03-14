9f94fbae0fbeed5d4c137cdb04faa41034514b5d
Michael Bestas <mkbestas@lineageos.org>
2022-03-14 20:43:12 -0300

    veux: Opt out of EPPE
    Change-Id: I71f54b1ad6de9d1406a3a55239acff1f4e426158

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -13,6 +13,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_DISABLE_EPPE := true
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
